50922e1810d3134c10ddbda4dbaaadb49721fdf7
Oleksandr Melnychuk <no-reply@mail.com>
2025-01-20 18:04:25 +0200

    chap1[1.2]: wip. add basic test bench

---
+++ Chapter_1_Introduction/Exercises/1.2/fourBitTest.sv
@@ -0,0 +1,4 @@
+`timescale 1ns/1ps
+
+module fourBitTest(output logic a, b, c, d);
+endmodule: fourBitTest
---
+++ Chapter_1_Introduction/Exercises/1.2/fourBitTest_tb.sv
@@ -0,0 +1,18 @@
+module fourBitTest_tb;
+
+logic a, b, c, d;
+
+// fourBitTest fb(.*);
+
+initial begin
+    $dumpfile("fourBitTest.vcd");
+    $dumpvars(0, fourBitTest_tb);
+    for (int i = 0; i < 16; i++) begin
+        {a, b, c, d} = i;
+        #1;
+        $display("Time=0%t a=%b b=%b c=%b d=%b", $time, a, b, c, d);
+    end
+    $finish;
+end
+
+endmodule: fourBitTest_tb;
